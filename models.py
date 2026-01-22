a128432e77a985f636ce02c7c951b8beb855fc4a
adnair2024 <anair@my.loyno.edu>
2026-01-22 17:26:45 -0600

    More fixing for sync

--- models.py
+++ models.py
@@ -124,7 +124,7 @@ class EventCompletion(db.Model):
 
 class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String(255), nullable=False)
+    message = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     is_read = db.Column(db.Boolean, default=False)
     type = db.Column(db.String(20), default='info')
